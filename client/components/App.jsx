6d75372c443f2eb34704afa50a9393c007f71343
waynetwleung <waynetwleung@gmail.com>
2023-04-29 15:08:07 -0400

    installed react router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import MapPage from './Pages/MapPage.jsx';
+import MainPage from './Pages/MainPage.jsx';
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 
 function App() {
-  return(
+  return (
     <div>
-      <h1>Yoo</h1>  
-      <MapPage />
+      <h1>Yoo</h1>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+        <Route path="/map" element={<MapPage />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ client/components/Pages/MainPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+function MainPage(props) {
+  return <h1>main Pages</h1>;
+}
+
+export default MainPage;
